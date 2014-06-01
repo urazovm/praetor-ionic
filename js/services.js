7e4a4bd05a63f09024ebfa08f3b3d769be093e11
Bohdan Maslowski <bohdan.maslowski@gmail.com>
2014-06-01 18:46:14 +0200

    hydration

--- js/services.js
+++ js/services.js
@@ -11,7 +11,23 @@ angular.module('starter.services', [])
     { id: 0, name: 'Scruff McGruff' },
     { id: 1, name: 'G.I. Joe' },
     { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
+    { id: 3, name: 'Ash Ketchum' },
+    { id: 4, name: 'Scruff McGruff' },
+    { id: 5, name: 'G.I. Joe' },
+    { id: 6, name: 'Miss Frizzle' },
+    { id: 7, name: 'Ash Ketchum' },
+    { id: 8, name: 'Scruff McGruff' },
+    { id: 9, name: 'G.I. Joe' },
+    { id: 12, name: 'Miss Frizzle' },
+    { id: 13, name: 'Ash Ketchum' },
+    { id: 10, name: 'Scruff McGruff' },
+    { id: 11, name: 'G.I. Joe' },
+    { id: 22, name: 'Miss Frizzle' },
+    { id: 23, name: 'Ash Ketchum' },
+    { id: 20, name: 'Scruff McGruff' },
+    { id: 31, name: 'G.I. Joe' },
+    { id: 32, name: 'Miss Frizzle' },
+    { id: 33, name: 'Ash Ketchum' },
   ];
 
   return {

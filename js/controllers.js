ad97a6df18107fe9f1a9abd2c18ad5c48660aa77
janhonza <jan.balcarik@deverus.sk>
2014-07-09 15:01:20 +0200

    WebApi

--- js/controllers.js
+++ js/controllers.js
@@ -1,117 +1,113 @@
 angular.module('starter.controllers', ['ui.utils'])
 
-.controller('AppCtrl', function($scope, $state) {
-  $scope.$root.canGoBack = true;
-  $scope.$root.sideMenuEnabled = false;//function() { return $state.current.name == 'app.home';} ;
+.controller('AppCtrl', function ($scope, $state) {
+    $scope.$root.canGoBack = true;
+    $scope.$root.sideMenuEnabled = false;//function() { return $state.current.name == 'app.home';} ;
 })
 
-.controller('DocumentCtrl', function($scope, $stateParams, $sce) {
-  $scope.document = $sce.trustAsResourceUrl('http://update.praetoris.cz/test/' + $stateParams.documentId);
+.controller('DocumentCtrl', function ($scope, $stateParams, $sce) {
+    $scope.document = $sce.trustAsResourceUrl('http://update.praetoris.cz/test/' + $stateParams.documentId);
 })
 
-.controller('HomeCtrl', function($scope, $location, $ionicLoading, praetorService) {
-  $scope.$root.canGoBack = false;
-  $scope.$root.sideMenuEnabled = true;
+.controller('HomeCtrl', function ($scope, $location, $ionicLoading, praetorService) {
+    $scope.$root.canGoBack = false;
+    $scope.$root.sideMenuEnabled = true;
 
-  $scope.recentItems = praetorService.recent;
+    $scope.spisy = praetorService.recent.spisy;
 
-  $scope.openSpis = function(spis) {
-    spis.loading = true;
-    //$ionicLoading.show({
-    // template: 'Loading...'
-    //});
-    praetorService.getSpis(spis.id).then(function(d) {
-      spis.loading = false;
-      //$ionicLoading.hide();
-      $location.path('/app/spis');
-    });
-  }
-})
-
-.controller('LoginCtrl', function($scope, $location, $ionicLoading, praetorService) {
-  
-  function doLogin()
-  {
-    $ionicLoading.show({
-      template: 'Loading...'
-    });
-    praetorService.login().then(function(d) {
-      $ionicLoading.hide();
-      if(d.success)
-      {
-        $location.path('/app/home');
-      }
-      else
-      {
-        $scope.message = d.message;
-      }
-    });
-  }
-  
-  $scope.$root.canGoBack = false;
-  $scope.$root.sideMenuEnabled = false;
-  var server = window.localStorage.getItem('server');
-  var username = window.localStorage.getItem('username');
-  var password = window.localStorage.getItem('password'); 
- 
-  if(server && username && password)
-  {
-    doLogin();
-  }
-  
-  $scope.formData = {
-    server: server || '',
-    username: username || '',
-    password: ''
-  };
-  
-
-  $scope.login = function() {
-    window.localStorage.setItem('server', $scope.formData.server);
-    window.localStorage.setItem('username', $scope.formData.username);
-    window.localStorage.setItem('password', $scope.formData.password);
-    doLogin();
-  }
-})
-
-.controller('SpisCtrl', function($scope, $location, praetorService, androidFileOpenerService) {
-  $scope.$root.sideMenuEnabled = false;
-
-  $scope.spis = praetorService.currentSpis;
-  
-  $scope.openDocument = function(file) {
-     
-
-    if(ionic.Platform.isAndroid()) {
-      androidFileOpenerService.downloadFile('http://update.praetoris.cz/test/'+file.name+'.'+file.extension, file.mime,'tmp001.' + file.extension);
+    $scope.openSpis = function (spis) {
+        spis.loading = true;
+        //$ionicLoading.show({
+        // template: 'Loading...'
+        //});
+        praetorService.getSpis(spis.id).then(function (d) {
+            spis.loading = false;
+            //$ionicLoading.hide();
+            $location.path('/app/spis');
+        });
     }
-    else
-      window.open(
-      'http://localhost:888/getFile/'+file.id,
-      '_blank',
-      'enableViewportScale=yes,location=no,toolbarposition=bottom,transitionstyle=fliphorizontal,hidden=no,closebuttoncaption=Zpět'
-      );
-  }
 })
 
-.controller('SideMenuCtrl', function($scope, $location, $state, $ionicSideMenuDelegate) {
+.controller('LoginCtrl', function ($scope, $location, $ionicLoading, praetorService) {
 
-  $scope.logout = function()
-  {
-    window.localStorage.removeItem('password');
-    $location.path('/login'); 
-  }
- // toggle side menu with Menu button
- document.addEventListener('menubutton', function () { $ionicSideMenuDelegate.toggleRight(); }, false);
- 
- // prevent returning to login page, close the app instead
- document.addEventListener('backbutton', function() { 
-  if($state.current.name == 'app.home')
-  {
-    // TODO: better stay at the home page
-    ionic.Platform.exitApp();
-  } 
-  } , false);
+    function doLogin(server, username, password) {
+        $ionicLoading.show({
+            template: 'Loading...'
+        });
+        praetorService.login(server, username, password).then(function (d) {
+            $ionicLoading.hide();
+            if (d.success) {
+                $location.path('/app/home');
+            }
+            else {
+                $scope.message = d.message;
+            }
+        });
+    }
+
+    $scope.$root.canGoBack = false;
+    $scope.$root.sideMenuEnabled = false;
+    var server = window.localStorage.getItem('server');
+    var username = window.localStorage.getItem('username');
+    var password = window.localStorage.getItem('password');
+
+    if (server && username && password) {
+        doLogin(server, username, password);
+    }
+
+    $scope.formData = {
+        server: server || '',
+        username: username || '',
+        password: ''
+    };
+
+
+    $scope.login = function () {
+        window.localStorage.setItem('server', $scope.formData.server);
+        window.localStorage.setItem('username', $scope.formData.username);
+        window.localStorage.setItem('password', $scope.formData.password);
+
+        doLogin($scope.formData.server, $scope.formData.username, $scope.formData.password);
+    }
+
+})
+
+.controller('SpisCtrl', function ($scope, $location, praetorService, androidFileOpenerService) {
+    $scope.$root.sideMenuEnabled = false;
+
+    $scope.spis = praetorService.currentSpis;
+
+    $scope.openDocument = function (file) {
+
+
+        if (ionic.Platform.isAndroid()) {
+            androidFileOpenerService.downloadFile('http://update.praetoris.cz/test/' + file.name + '.' + file.extension, file.mime, 'tmp001.' + file.extension);
+        }
+        else
+            window.open(
+            'http://localhost:888/getFile/' + file.id,
+            '_blank',
+            'enableViewportScale=yes,location=no,toolbarposition=bottom,transitionstyle=fliphorizontal,hidden=no,closebuttoncaption=Zpět'
+            );
+    }
+})
+
+.controller('SideMenuCtrl', function ($scope, $location, $state, $ionicSideMenuDelegate) {
+
+    $scope.logout = function () {
+        window.localStorage.removeItem('password');
+        $location.path('/login');
+    }
+    // toggle side menu with Menu button
+    document.addEventListener('menubutton', function () { $ionicSideMenuDelegate.toggleRight(); }, false);
+
+    // prevent returning to login page, close the app instead
+    document.addEventListener('backbutton', function () {
+        if ($state.current.name == 'app.home') {
+            // TODO: better stay at the home page
+            ionic.Platform.exitApp();
+        }
+    }, false);
 
 
 
--- js/praetorService.js
+++ js/praetorService.js
@@ -1,92 +1,98 @@
 
 angular.module('starter.praetorService', [])
-  .factory('praetorService', function($http) {
-  var instance = {
-    
-    // static data
-    recent : {},
-    currentSpis : {},
+  .factory('praetorService', function ($http) {
+      var instance = {
 
-    // calls 
-    login: function() {
-      var promise = $http.get('http://localhost:888/login')
-      .then(function (response) {
-       if(response.data.success)
-        {
-          instance.recent = response.data.recent
-        }
-        return response.data;
-      })
-      .catch(function(e) {
-        return { success: false, message: "Error " + e.status };
-      });
-      return promise;
-    },
-    
-    getSpis: function(id) {
-      console.log("getSpis "+id);
-      var promise = $http.post('http://localhost:888/getSpis?callback=JSON_CALLBACK', { id: id }).then(function (response) {
-        //console.log(response.data);
-        instance.currentSpis = response.data;
-        return response.data;
-      });
-      return promise;
-    },
-    
-    
-    
-  };
-  return instance;
-})
+          // static data
+          recent: {},
+          currentSpis: {},
 
-  .factory('androidFileOpenerService', function($http) {
-     
-     var instance = {
-      openIntent: function(url, mime)
-      {
-        window.plugins.webintent.startActivity({
-            action: window.plugins.webintent.ACTION_VIEW,
-            url: url,
-            type: mime
+          // calls 
+          login: function (server, username, password) {
+              //var promise = $http.get('http://' + server + ':8080/api/praetorionic/getlogin?username=' + username + '&password=' + password + '')
+
+              var requestData = {"username":username, "password":password}
+
+              var promise = $http.post(
+                  'http://' + server + ':8080/praetorapi/login',
+                  requestData,
+                  { headers: { 'Content-Type': 'application/json' } })
+            .then(function (response) {
+                debugger;
+                if (response.data.success) {
+                    instance.recent = response.data
+                }
+                return response.data;
+            })
+            .catch(function (e) {
+                return { success: false, message: "Error " + e.status };
+            });
+              return promise;
           },
-          function() {},
-          function(x) {
-            alert(x);
-            alert('Failed to open URL via Android Intent.');
-            console.log("Failed to open URL via Android Intent.")
+
+          getSpis: function (id) {
+              console.log("getSpis " + id);
+              var promise = $http.post('http://localhost:888/getSpis?callback=JSON_CALLBACK', { id: id }).then(function (response) {
+                  //console.log(response.data);
+                  instance.currentSpis = response.data;
+                  return response.data;
+              });
+              return promise;
+          },
+
+
+
+      };
+      return instance;
+  })
+
+  .factory('androidFileOpenerService', function ($http) {
+
+      var instance = {
+          openIntent: function (url, mime) {
+              window.plugins.webintent.startActivity({
+                  action: window.plugins.webintent.ACTION_VIEW,
+                  url: url,
+                  type: mime
+              },
+                function () { },
+                function (x) {
+                    alert(x);
+                    alert('Failed to open URL via Android Intent.');
+                    console.log("Failed to open URL via Android Intent.")
+                }
+              );
+          },
+
+          downloadFile: function instance(fileUrl, mimeType, tempName) {
+
+              window.requestFileSystem(LocalFileSystem.PERSISTENT, 0,
+                function onFileSystemSuccess(fileSystem) {
+                    fileSystem.root.getFile(
+                    "dummy.html", { create: true, exclusive: false },
+                    function gotFileEntry(fileEntry) {
+                        sPath = fileEntry.toNativeURL().replace("dummy.html", "");
+
+                        var fileTransfer = new FileTransfer();
+                        fileEntry.remove();
+
+                        fileTransfer.download(
+                            fileUrl,
+                            sPath + tempName,
+                            function (theFile) {
+                                service.openIntent(theFile.toNativeURL(), mimeType);
+                            },
+                            function (error) {
+                                alert("download error source " + error.source);
+                                alert("download error target " + error.target);
+                                alert("upload error code: " + error.code);
+                            }
+                        );
+                    }, function (e) { alert(e); });
+                }, function (e) { alert(e); });
           }
-        );
-      },
-
-      downloadFile: function instance(fileUrl, mimeType, tempName){
-
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-          function onFileSystemSuccess(fileSystem) {
-              fileSystem.root.getFile(
-              "dummy.html", {create: true, exclusive: false}, 
-              function gotFileEntry(fileEntry) {
-                  sPath = fileEntry.toNativeURL().replace("dummy.html","");
-                                    
-                  var fileTransfer = new FileTransfer();
-                  fileEntry.remove();
-      
-                  fileTransfer.download(
-                      fileUrl,
-                      sPath + tempName,
-                      function(theFile) {
-                          service.openIntent(theFile.toNativeURL(), mimeType);
-                      },
-                      function(error) {
-                          alert("download error source " + error.source);
-                          alert("download error target " + error.target);
-                          alert("upload error code: " + error.code);
-                      }
-                  );
-              }, function(e) { alert(e); } );
-          }, function(e) { alert(e); });
-      }
-    };
-    return instance;
+      };
+      return instance;
   });
 
 

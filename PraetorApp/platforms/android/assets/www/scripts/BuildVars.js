b6807edcbf668ec54a2c8572fe47e1ea63956a87
Jan Balcařík <jan.balcarik@deverus.sk>
2015-07-05 23:24:59 +0200

    Odstranění adresáře platforms

--- PraetorApp/platforms/android/assets/www/scripts/BuildVars.js
+++
@@ -1,1 +0,0 @@
-window.buildVars = {debug: 'debug' == 'debug',buildTimestamp: '5. 7. 2015 22:31:11',majorVersion: 1,minorVersion: 0,buildVersion: 30};
--- PraetorApp/www/scripts/BuildVars.js
+++ PraetorApp/www/scripts/BuildVars.js
@@ -1,1 +1,1 @@
-window.buildVars = {debug: 'debug' == 'debug',buildTimestamp: '5. 7. 2015 22:31:11',majorVersion: 1,minorVersion: 0,buildVersion: 30};
+window.buildVars = {debug: 'debug' == 'debug',buildTimestamp: '5. 7. 2015 23:24:02',majorVersion: 1,minorVersion: 0,buildVersion: 31};

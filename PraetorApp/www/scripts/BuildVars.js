0b96f0f53013dccceb7607f3926103e371eaf07d
Jan Balcařík <jan.balcarik@deverus.sk>
2015-07-05 23:26:46 +0200

    Přidání do ignor listu

--- PraetorApp/www/scripts/BuildVars.js
+++ PraetorApp/www/scripts/BuildVars.js
@@ -1,1 +1,1 @@
-window.buildVars = {debug: 'debug' == 'debug',buildTimestamp: '5. 7. 2015 23:24:02',majorVersion: 1,minorVersion: 0,buildVersion: 31};
+window.buildVars = {debug: 'debug' == 'debug',buildTimestamp: '5. 7. 2015 23:25:35',majorVersion: 1,minorVersion: 0,buildVersion: 31};

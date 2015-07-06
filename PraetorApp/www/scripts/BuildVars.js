1676fa71ae32417b532f00c21d69e6a61a0c1c44
Jan Balcařík <jan.balcarik@deverus.sk>
2015-07-06 12:27:55 +0200

    Nastavení uvítací obrazovky plus ikony

--- PraetorApp/www/scripts/BuildVars.js
+++ PraetorApp/www/scripts/BuildVars.js
@@ -1,1 +1,1 @@
-window.buildVars = {debug: 'debug' == 'debug',buildTimestamp: '6. 7. 2015 11:42:49',majorVersion: 1,minorVersion: 0,buildVersion: 52};
+window.buildVars = {debug: 'debug' == 'debug',buildTimestamp: '6. 7. 2015 12:27:15',majorVersion: 1,minorVersion: 0,buildVersion: 53};

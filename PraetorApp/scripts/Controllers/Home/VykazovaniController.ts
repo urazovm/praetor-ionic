ca5c6dd6bd20959d37c2fc9ccd1dd5dc500a74f5
Bohdan Maslowski <bohdan.maslowski@gmail.com>
2015-07-07 20:58:55 +0200

    documentHandler

--- PraetorApp/scripts/Controllers/Home/VykazovaniController.ts
+++ PraetorApp/scripts/Controllers/Home/VykazovaniController.ts
@@ -4,25 +4,24 @@
 
         public static ID = "HomeVykazovaniController";
 
-        public static get $inject(): string[] {
-            return ["$scope", "$location", "$http", Services.Utilities.ID, Services.UiHelper.ID, Services.Preferences.ID];
+        public static get $inject(): string[]{
+            return ["$scope", Services.PraetorService.ID];
         }
 
-        private $location: ng.ILocationService;
-        private $http: ng.IHttpService;
-        private Utilities: Services.Utilities;
-        private UiHelper: Services.UiHelper;
-        private Preferences: Services.Preferences;
+        private PraetorService: Services.PraetorService;
 
-        constructor($scope: ng.IScope, $location: ng.ILocationService, $http: ng.IHttpService, Utilities: Services.Utilities, UiHelper: Services.UiHelper, Preferences: Services.Preferences) {
+        constructor($scope: ng.IScope, praetorService: Services.PraetorService) {
             super($scope, ViewModels.Home.VykazovaniViewModel);
 
-            this.$location = $location;
-            this.$http = $http;
-            this.Utilities = Utilities;
-            this.UiHelper = UiHelper;
-            this.Preferences = Preferences;                        
+            this.PraetorService = praetorService;                        
         }
+
+        public test(url)
+        {
+            this.PraetorService.openFile(url);
+        }
+
+
 
     }
 }

6fc74659b0283abbcb2cd459d949d1cd5b59143f
Jan Balcařík <jan.balcarik@deverus.sk>
2015-07-09 03:03:22 +0200

    Oprava filteru

--- PraetorApp/scripts/Filters/PrehledSpisuFilter.ts
+++ PraetorApp/scripts/Filters/PrehledSpisuFilter.ts
@@ -1,18 +1,5 @@
 ﻿module PraetorApp.Filters {
 
-    /**
-     * Formats numbers greater than one thousand to include the K suffix.
-     * 
-     * Numbers greater than 10,000 will not show decimal places, while numbers
-     * between 1,000 and 9,999 will show decimal places unless the number is
-     * a multiple of one thousand.
-     * 
-     * For example:
-     *      200   -> 200
-     *      2000  -> 2K
-     *      1321  -> 1.3K
-     *      10700 -> 10K
-     */
     export class PrehledSpisuFilter {
 
         public static ID = "PrehledSpisuFilter";
@@ -47,7 +34,7 @@
                 if (jeTam)
                     out.push(row);
 
-                if (out.length >= 20)
+                if (out.length >= 50)
                     return;
             });
 

056e650cb6543cd1a867b25a0ea097571f9794d1
Bohdan Maslowski <bohdan.maslowski@gmail.com>
2015-07-10 14:34:36 +0200

    tel

--- PraetorApp/www/scripts/BuildVars.js
+++ PraetorApp/www/scripts/BuildVars.js
@@ -1,1 +1,1 @@
-window.buildVars = {debug: 'debug' == 'debug',buildTimestamp: '10.7.2015 14:12:33',majorVersion: 1,minorVersion: 0,buildVersion: 140};
+window.buildVars = {debug: 'debug' == 'debug',buildTimestamp: '10.7.2015 14:31:04',majorVersion: 1,minorVersion: 0,buildVersion: 140};
